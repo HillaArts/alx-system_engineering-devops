6b5110ead67ed42377c2ae5802a26d42b53ecb9f
HillaArts <hassenyehillary@gmail.com>
2024-03-11 09:10:05 -0700

    install load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,9 @@
-#!/usr/bin/env bash
-#Class nginx_custom_header {
-  # Ensure Nginx is installed
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Fetch the hostname of the server
-  $hostname = $::hostname
-
-  # Define the custom header configuration
-  $custom_header = "add_header X-Served-By '${hostname}';\n"
-
-  # File resource to configure Nginx
-  file { '/etc/nginx/conf.d/custom_header.conf':
-    ensure  => file,
-    content => $custom_header,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['nginx'], # Ensure Nginx is installed before modifying its configuration
-    notify  => Service['nginx'], # Reload Nginx to apply the configuration changes
-  }
-
-  # Ensure Nginx service is running and enabled to start at boot
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/conf.d/custom_header.conf'], # Reload Nginx if the custom configuration changes
-  }
+# automate the task of creating a custom HTTP header response
+# Manifest for creating a custom HTTP header response
+exec { 'http header':
+  command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "s/server_name _;/server_name _;\n\n\tadd_header X-Served-By $HOSTNAME;\n/" /etc/nginx/sites-enabled/default;
+	sudo service nginx restart',
+  provider => shell,
 }
-
-include nginx_custom_header
-
