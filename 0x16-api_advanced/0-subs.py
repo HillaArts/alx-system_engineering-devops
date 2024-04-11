de793dc53affd2824f3526fa079223cdaf6a772c
HillaArts <hassenyehillary@gmail.com>
2024-04-11 13:08:27 -0700

    update advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 """
-import module
+a function that queries a subreddit andreturns no of subs
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    function that returns the total number of
-    subscribers for a given subreddit.
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
     """
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
-
-    user_agent = {'User-Agent': 'Python/requests'}
-
-    response = requests.get(api_uri, headers=user_agent,
-                            allow_redirects=False)
-
-    if response.status_code in [302, 404]:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
         return 0
-
-    return response.json().get('data').get('subscribers')
